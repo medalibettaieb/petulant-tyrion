564f26e712d93b9cc3e9d17c20aa8e7e904e2815
labo1 <labo1@192.168.92.244>
2013-11-30 10:35:15 +0100

    Signed-off-by: labo1 <labo1@192.168.92.244>

---
+++ fibonacci/src/test/java/edu/esprit/teemJee/FibonacciTest.java
@@ -0,0 +1,20 @@
+package edu.esprit.teemJee;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FibonacciTest {
+	private Fibonacci fibonacci;
+
+	@BeforeMethod
+	public void setUp() {
+		fibonacci = new Fibonacci();
+	}
+
+	@Test
+	public void shouldReturnOneWhenOneIn() {
+		Assert.assertEquals(1, fibonacci.fibo(1));
+
+	}
+}
