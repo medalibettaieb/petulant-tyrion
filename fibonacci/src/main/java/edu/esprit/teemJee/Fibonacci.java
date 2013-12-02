c8c8bcf08df3ac16b0d1654e4607ae872b0f5204
labo1 <labo1@192.168.92.244>
2013-12-02 08:51:38 +0100

    Signed-off-by: labo1 <labo1@192.168.92.244>

--- fibonacci/src/main/java/edu/esprit/teemJee/Fibonacci.java
+++ fibonacci/src/main/java/edu/esprit/teemJee/Fibonacci.java
@@ -1,10 +1,11 @@
 package edu.esprit.teemJee;
 
 public class Fibonacci {
-	public long fibo(long l) {
+	public  long fibo(long l) {
 		if (l <= 1)
 
 			return l;
 		return fibo(l - 1) + fibo(l - 2);
 	}
+	
 }
